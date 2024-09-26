528fe6c88d6566fea70136f136f380430668b2eb
MrTheoW <t.willemse@gmail.com>
2024-09-27 01:42:32 +0200

    Update FileRenamer.ps1
    Added some comments in the script to explain what it does.

--- FileRenamer.ps1
+++ FileRenamer.ps1
@@ -1,7 +1,7 @@
 (ls) | foreach {
-    $oldfilename = $_.name
-    $FileExtention = "."+($oldfilename).Split(".")[-1]
-    $newfilename = $(([System.IO.Path]::GetRandomFileName()).split(".")[0])+$FileExtention
-    mv $_ $($newfilename)
-    Write-Host "$oldfilename > $newfilename"
+    $oldfilename = $_.name                                                                    # Grabs the filename of the current file.
+    $FileExtention = "."+($oldfilename).Split(".")[-1]                                        # Grabs the extention from the oldfilename variable.
+    $newfilename = $(([System.IO.Path]::GetRandomFileName()).split(".")[0])+$FileExtention    # Create a randomized filename, add the new extention and put it in the "newfilename" variable.
+    mv $_ $($newfilename)                                                                     # Rename the file by moving the old filename to the new one. 
+    Write-Host "$oldfilename > $newfilename"                                                  # Outputs what it has done.
 }
